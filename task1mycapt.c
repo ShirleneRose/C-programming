f0d53574cb1a09f9eead6bb0ac7945ff45a96848
ShirleneRose <51985847+ShirleneRose@users.noreply.github.com>
2019-11-24 11:53:08 +0530

    Add files via upload

---
+++ task1mycapt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char name;
+	int age ; long int phno;
+	printf("Please Enter Your name");
+	scanf("%s",&name);
+	printf("Pleae enter your age");
+	scanf("%d",&age);
+	printf("Please enter your phone number");
+	scanf("%d",&phno);
+	printf("Your name is %s\n Your age is %d\n Your phone number is %d",name,age,phno);
+	
+}
+
